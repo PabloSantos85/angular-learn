9a77ba56148e4ccde218558337d5b4fe7dd5f891
Pablosantos85 <pablossantos85@gmail.com>
2021-05-19 22:17:11 -0300

    novos componentes, atualizado componente home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { HomeComponent } from './components/home/home.component';
+import { GaleryComponent } from './components/galery/galery.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CarouselComponent,
+    HomeComponent,
+    GaleryComponent
   ],
   imports: [
     BrowserModule,
